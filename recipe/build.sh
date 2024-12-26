e54937166c168939b8e48677ed6cb1c950b6088b
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-26 01:21:26 -0600

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,7 @@ export DISABLE_AUTOBREW=1
 mv DESCRIPTION DESCRIPTION.old
 grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
-${R} CMD INSTALL --build . ${R_ARGS}
+# Extra configure-arg is neede because configure script expects source
+# to be in a folder called "rayrender" - setting ac_unique_file to ""
+# just makes it look in $SRC_DIR where the source actually resides
+${R} CMD INSTALL --build . ${R_ARGS} --configure-args='ac_unique_file=""'
