656db48f70530ab4c4ceabcbfd31b97272d49230
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-26 01:56:10 -0600

    set c_stdlib to 10.15

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,5 +8,4 @@ grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
 # Extra configure-arg is neede because configure script expects source
 # to be in a folder called "rayrender" - setting ac_unique_file to ""
 # just makes it look in $SRC_DIR where the source actually resides
-export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 ${R} CMD INSTALL --build . ${R_ARGS} --configure-args='ac_unique_file=""'
