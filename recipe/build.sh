286e724572c823b33bc1a02ebb756597df0acb6b
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-26 12:04:11 -0600

    disable libcpp availability

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ mv DESCRIPTION DESCRIPTION.old
 grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
 sed -i 's/-mmacosx-version-min=10.13//g' ${PREFIX}/lib/R/etc/Makeconf
-export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+export CPPFLAGS="${CPPFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 # Extra configure-arg is neede because configure script expects source
 # to be in a folder called "rayrender" - setting ac_unique_file to ""
 # just makes it look in $SRC_DIR where the source actually resides
